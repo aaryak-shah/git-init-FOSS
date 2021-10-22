fb669a7ec4387f0edb8847d6fd0dbcf0d67bad6f
AmanPachori <amangirrajpachori2002@gmail.com>
2021-10-22 09:43:57 +0530

    changed PWA colour and name

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = "version-1";
-const urlsToCache = [ 'index.html', 'offline.html' ];
+const urlsToCache = [  './index.html', './offline.html','' ];
 
 const self = this;
 
@@ -9,13 +9,10 @@ self.addEventListener('install', (event) => {
         caches.open(CACHE_NAME)
             .then((cache) => {
                 console.log('Opened cache');
-
                 return cache.addAll(urlsToCache);
             })
     )
 });
-
-
 self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request)
